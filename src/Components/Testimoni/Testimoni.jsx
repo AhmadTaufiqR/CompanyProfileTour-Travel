045bbd4916d0dc6de074eed78b648c6021d35592
AhmadTaufiqR <taufiqridhoi423@gmail.com>
2024-08-01 23:13:08 +0700

    Add: view Testimoni with the json value

--- src/Components/Testimoni/Testimoni.jsx
+++ src/Components/Testimoni/Testimoni.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import TestimoniResources from "../../Resources/TestimoniResouces";
 
 const Testimoni = () => {
   return (
     <>
-    
+      <div className="container">
+        <div className="min-h-[600px] flex flex-wrap py-10">
+          <h1 className="text-5xl font-poppins text-primary text-center mt-20 flex items-center justify-center w-[100%]">
+            Testimoni
+          </h1>
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 mt-10">
+            {TestimoniResources.map(({id, name, testimoni}) => (
+              <div className="bg-gray-200 p-4 rounded-md" key={id}>
+                <p className="text-gray-500">
+                  {testimoni}
+                </p>
+                <h1 className="text-primary font-bold mt-4">- {name}</h1>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/Resources/TestimoniResouces.jsx
@@ -0,0 +1,19 @@
+const TestimoniResources = [
+    {
+        id: 1,
+        name: "Taufiq Ridho'i",
+        testimoni: "Sangat menyenangkan menggunakan tour and travel ini"
+    },
+    {
+        id: 2,
+        name: "Ahmad Taufiq",
+        testimoni: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan odio. Integer sit amet nisi ac nisl tincidunt scelerisque. Nullam auctor, nisl ac laoreet tincidunt, nunc libero ultricies risus, quis tincidunt odio mi at nunc. Ut tincidunt, nisl nec lacinia."
+    },
+    {
+        id: 3,
+        name: "Yessa Amelia",
+        testimoni: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis accumsan odio. Integer sit amet nisi ac nisl tincidunt scelerisque. Nullam auctor, nisl ac laoreet tincidunt, nunc libero ultricies risus, quis tincidunt odio mi at nunc. Ut tincidunt, nisl nec lacinia."
+    }
+]
+
+export default TestimoniResources;
