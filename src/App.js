47a0a16120e7dcdb0d1af7b4efa33e2f16c216e2
AhmadTaufiqR <taufiqridhoi423@gmail.com>
2024-08-02 17:22:53 +0700

    Adding: a contact view, footer, and plan view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import About from "./Components/About/About";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Testimoni from "./Components/Testimoni/Testimoni";
+import Plan from "./Components/Plan/Plan.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import Contacts from "./Components/Contact/Contacts.jsx";
 
 
 const App = () => {
@@ -12,6 +15,9 @@ const App = () => {
       <Hero />
       <About />
       <Testimoni/>
+      <Plan />
+      <Contacts />
+      <Footer />
     </>
   );
 };
